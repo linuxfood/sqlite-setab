1138563ae6162df10d283549edf52c2a9f2e998d
Brian Smith <brian@linuxfood.net>
2016-03-16 06:02:07 +0000

    Add setup for dependencies
    * Add BUCK files for external dependencies.
    * Add buck targets and setup scripts to support building.

---
+++ build/setup_symlinks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+
+FLOOP="${1}"
+OUT="${2}"
+ROOT="$(buck root)"
+
+for d in ${ROOT}/externals/*; do
+    rm -f $d/include && \
+    rm -f $d/lib && \
+    ln -s  $FLOOP/include $d/include && \
+    ln -s  $FLOOP/lib     $d/lib
+done
+
+if [[ "${FLOOP:0:4}" = "/nix" ]]; then
+    ln -s $ROOT/build/nix $ROOT/.buckconfig.local
+else
+    ln -s $ROOT/build/ubuntu $ROOT/.buckconfig.local
+fi
+
+touch $OUT
